95e38ab140ec4f7edee8b96db86c37c926c676b6
Adelin Niculae <adelin.nic3@gmail.com>
2022-07-05 23:23:36 +0300

    updated interface for challenge 6

--- vueLearn/src/components/random-generator.vue
+++ vueLearn/src/components/random-generator.vue
@@ -1,41 +1,69 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 
+interface RandAPIResponse {numbers: number[],timestamp: string}
+
 export default defineComponent({
     props: {
         apiURL: String
     },
     data() {
         return {
-            randArr: [],
             randRespDotNet: "",
             randRespNodeJS: "",
             dotnetTime: 0,
             nodejsTime: 0,
             APICallsNumber: 1,
-            simultaneousRequests: 1
+            simultaneousRequests: 1,
+            table: [[0]]
         }
     },
     methods: {
-        async fetchRand() {
-            interface RandAPIResponse {numbers: number[],timestamp: string}
+        async callNetAPI(i: number) {
             let startTime = performance.now();
-            this.randArr = await fetch(import.meta.env.VITE_DOTNET_API+'random').then(x=>x.json());
-            this.randRespDotNet = '['+((this.randArr as unknown) as RandAPIResponse[]).map(x=>x.numbers.join(',')).join('],[')+']';
+            let randArr: RandAPIResponse[];
+            try{
+                randArr = await fetch(import.meta.env.VITE_DOTNET_API+'random').then(x=>x.json());
+            }catch{
+                randArr = await fetch(import.meta.env.VITE_DOTNET_API+'random').then(x=>x.json());
+            }
+            this.randRespDotNet = '['+randArr.map(x=>x.numbers.join(',')).join('],[')+']';
             this.dotnetTime = performance.now()-startTime;
-
-            startTime = performance.now();
-            this.randArr = await fetch(import.meta.env.VITE_NODE_API+'random').then(x=>x.json());
-            this.randRespNodeJS = '['+((this.randArr as unknown) as RandAPIResponse[]).map(x=>x.numbers.join(',')).join('],[')+']';
+            this.table[i-1][0]=this.dotnetTime;
+        },
+        async callNodeAPI(i: number) {
+            let startTime = performance.now();
+            let randArr: RandAPIResponse[];
+            try{
+                randArr = await fetch(import.meta.env.VITE_NODE_API+'random').then(x=>x.json());
+            }catch{
+                randArr = await fetch(import.meta.env.VITE_NODE_API+'random').then(x=>x.json());
+            }
+            this.randRespNodeJS = '['+randArr.map(x=>x.numbers.join(',')).join('],[')+']';
             this.nodejsTime = performance.now()-startTime;
+            this.table[i-1][1]=this.nodejsTime;
+        },
+        async fetchRand() {
+            let queue: Promise<void>[][] = [];
+            this.table = []; (<any>window)._table=this.table; (<any>window)._calls=this.APICallsNumber;
+            if(this.simultaneousRequests<1) throw new Error("There should be at least one simultaneous request... "+this.simultaneousRequests+" provided.");
+            for(let i=1;i<=this.APICallsNumber;i++){
+                queue.push([this.callNetAPI(i),this.callNodeAPI(i)]);
+                this.table.push([]);
+                if(i%this.simultaneousRequests==0) {
+                    await Promise.all(queue.map(x=>Promise.all(x)));
+                    queue = [];
+                }
+            }
+            await Promise.all(queue.map(x=>Promise.all(x)));
         }
     }
 })
 </script>
 <template>
     <div id="settings">
-        Number of API calls:<br><input type="text" v-model="APICallsNumber">
-        Simultaneous requests:<br><input type="text" v-model="simultaneousRequests">
+        Number of API calls:<br><input type="text" v-model="APICallsNumber" placeholder="calls"><br>
+        Simultaneous requests:<br><input type="text" v-model="simultaneousRequests" placeholder="reqs">
     </div>
     <button @click="fetchRand">Fetch</button>
     <div v-if="randRespDotNet" id="dotnet" class="numbers">
@@ -48,11 +76,25 @@ export default defineComponent({
         <p>Response time: {{nodejsTime}}ms</p>
         <p>{{randRespNodeJS}}</p>
     </div>
+    <div id="results">
+        <table>
+            <tr>
+                <th>DotNet</th>
+                <th>NodeJS</th>
+            </tr>
+            <tr v-for="row in table">
+                <td>{{row[0]}}</td>
+                <td>{{row[1]}}</td>
+            </tr>
+        </table>
+    </div>
 </template>
 <style>
 .numbers {
-    max-height: 50%;
-    overflow-y: scroll;
-    overflow-x: hidden;
+    max-height: 30%;
+    overflow: scroll;
+}
+#settings > input {
+    width: 90px;
 }
 </style>
