1d97f8431706152d50102c3de5273d396983a5b5
Adelin Niculae <adelin.nic3@gmail.com>
2022-07-08 22:11:01 +0300

    prepare for loaderio

---
+++ dotNetLearn/Application/Controllers/LodaerioController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using dotNetLearn.Services;
+using System.Net;
+
+namespace dotNetLearn.Controllers;
+
+[ApiController]
+[Route("loaderio-9ddd6b937bd35ed2843a048a2b73d085")]
+public class LoaderioController : ControllerBase
+{
+
+    private readonly ILogger<RandommController> _logger;
+    private RandService randGenerator;
+
+    public LoaderioController(ILogger<RandommController> logger, RandDBContext context){
+        _logger = logger;
+        randGenerator = new RandService(context);
+    }
+
+    [EnableCors("frontend")]
+    [HttpGet(Name = "GetRandom")]
+    public HttpResponseMessage Get() {
+        var resp = new HttpResponseMessage(HttpStatusCode.OK);
+        resp.Content = new StringContent("loaderio-9ddd6b937bd35ed2843a048a2b73d085", System.Text.Encoding.UTF8, "text/plain");
+        return resp;
+    }
+}
