40f66b843f8e25fd91280d6034ee3e09c2fb14f6
Adelin Niculae <adelin.nic3@gmail.com>
2022-07-08 21:15:40 +0300

    again

--- vueLearn/env.d.ts
+++ vueLearn/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_DOTNET_API: string,
+    readonly VITE_NODE_API: string,
+    readonly VITE_BASE: string
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
--- vueLearn/vite.config.ts
+++ vueLearn/vite.config.ts
@@ -3,6 +3,8 @@ import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+let b = process.env.BASE||'/learning-vue-frontend/dist/';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -11,5 +13,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: './'
+  base: b
 })
