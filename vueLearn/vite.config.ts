7eb071f211a7f93d359aff84a3de010a982fbd08
Adelin Niculae <adelin.nic3@gmail.com>
2022-07-08 21:17:48 +0300

    back

--- vueLearn/vite.config.ts
+++ vueLearn/vite.config.ts
@@ -3,8 +3,6 @@ import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-let b = process.env.BASE||'/learning-vue-frontend/dist/';
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -13,5 +11,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: b
+  base: ''
 })
