cc3fa1106558a38a0be6ea45da6eb2601e1e9597
Adelin Niculae <adelin.nic3@gmail.com>
2022-07-13 20:37:04 +0300

    b

---
+++ nodejsLearn/src/index.js
@@ -0,0 +1,10 @@
+// import express from '@feathersjs/express';
+// const app = express();
+// const server = app.listen(3030);
+// process.on('unhandledRejection', (reason, p) =>
+//   console.error('Unhandled Rejection at: Promise ', p, reason)
+// );
+// server.on('listening', () =>
+//   console.info('Feathers application started on http://%s:%d', "host.example.com", 3030)
+// );
+console.log("it works");
