516e725a24aa992c485a8c9b1596394038507487
Adelin Niculae <adelin.nic3@gmail.com>
2022-07-14 12:26:49 +0300

    add logging

--- nodejsLearn/src/index.ts
+++ nodejsLearn/src/index.ts
@@ -1,6 +1,16 @@
+let start: number;
+let trackStartup: Function;
+//import appInsights from 'applicationinsights';
 if(process.env.APPLICATIONINSIGHTS_CONNECTION_STRING) {
   const appInsights = require('applicationinsights');
-  appInsights.setup(process.env.APPLICATIONINSIGHTS_CONNECTION_STRING).start();
+  appInsights.setup(process.env.APPLICATIONINSIGHTS_CONNECTION_STRING);
+  appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = "NodeJS-API";
+  appInsights.start();
+  start = Date.now();
+  trackStartup = function(){
+    let duration = Date.now() - start;
+    appInsights.defaultClient.trackMetric({name: "server startup time", value: duration});
+  }
 }
 import logger from './logger';
 import app from './app';
@@ -13,6 +23,9 @@ process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
 );
 
-server.on('listening', () =>
-  logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
-);
+server.on('listening', () => {
+  if(start) {
+    trackStartup();
+  }
+  return logger.info('Feathers application started on http://%s:%d', app.get('host'), port);
+});
