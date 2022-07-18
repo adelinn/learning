89bc8acb84555997edcf6a1e1b6f564477900a91
Adelin Niculae <adelin.nic3@gmail.com>
2022-07-18 17:01:10 +0300

    asd

--- nodejsLearn/src/iEnv.ts
+++ nodejsLearn/src/iEnv.ts
@@ -3,8 +3,4 @@ if(process.env.APPLICATIONINSIGHTS_CONNECTION_STRING) ENV = 'Azure';
 else if(process.env.PORT && process.env.INSTANCE_CONNECTION_NAME) ENV = 'GCP';
 else ENV = 'Other';
 
-let e = '';
-for(let i in process.env) e+=i+': '+process.env[i]+';';
-console.log(Object.keys(process.env).join(' ')+' '+e+" aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-
 export default ENV;
