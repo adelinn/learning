f19be1cdc570b89512d9e513d96e3c6e5b4937d0
Adelin Niculae <adelin.nic3@gmail.com>
2022-07-19 10:48:56 +0300

    header error

--- dotNetLearn/Application/Program.cs
+++ dotNetLearn/Application/Program.cs
@@ -13,7 +13,7 @@ if((Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING")?
     toLog+="Got connection string.\n";
     builder.Services.AddApplicationInsightsTelemetry(Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING"));
 } else if((Environment.GetEnvironmentVariable("PORT")??"")!=""){ //GCP
-    builder.Services.AddGoogleDiagnosticsForAspNetCore();
+    //builder.Services.AddGoogleDiagnosticsForAspNetCore();
     builder.Services.AddGoogleTraceForAspNetCore();
     //builder.WebHost.ConfigureLogging(builder => builder.AddGoogle(new LoggingServiceOptions {}));
 }
